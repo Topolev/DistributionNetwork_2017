bd3cfdb1fa04a89f04b3e0af0166a5d5a7319fae
Topolev <i.topolev.vladimir@gmail.com>
2017-05-21 15:25:42 +0300

    Initial commit

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,12 @@ import { RouterModule } from '@angular/router';
 import { DistributionNetworkSharedModule } from '../shared';
 
 import { HOME_ROUTE, HomeComponent } from './';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
     imports: [
         DistributionNetworkSharedModule,
+        NgbModule,
         RouterModule.forRoot([ HOME_ROUTE ], { useHash: true })
     ],
     declarations: [
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -65,7 +65,7 @@ module.exports = function (options) {
                 },
                 {
                     test: /\.(jpe?g|png|gif|svg|woff2?|ttf|eot)$/i,
-                    loaders: ['file-loader?hash=sha512&digest=hex&name=[hash].[ext]']
+                    loaders: ['file-loader?hash=sha512&digest=hex&name=/images/[hash].[ext]']
                 },
                 {
                     test: /app.constants.ts$/,
